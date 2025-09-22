2be7f2d4bd19562777d1b1489ba582756f81d7e5
ewnice <eunicechan26@crosspointacademy.org>
2025-09-22 20:38:49 +0000

    MCQ

---
+++ MCQ.java
@@ -0,0 +1,17 @@
+//Eunice Chan
+//2025-09-22
+
+public class MCQ {
+    public static void main(String[] args) {
+
+        //question 1: D
+
+        //question 2: D
+        
+        //question 3: A
+
+        //question 4: E
+    
+        //question 5: E
+    }
+}
